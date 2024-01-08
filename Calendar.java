c1824b58c1cc21e708c59a372f43b34584378f96
Noale4444 <noa.bendor@post.runi.ac.il>
2024-01-08 13:52:55 +0200

    Add awesome code for Noale4444

--- Calendar.java
+++ Calendar.java
@@ -1,94 +1,83 @@
-
-public class Calendar {	
+public class Calendar {    
   
-	static int curMonth;
-	static int curDay;
-	static int curYear;
-	static int endYear = 2000; 
-	static int curDayOfWeek; 
-	static int nDaysInMonth; 
-	static boolean isLeapYear; 
-	static int nDays; 
-	static int countSunday = 0;
+    static int curMonth;
+    static int curDay;
+    static int curYear;
+    static int curDayOfWeek; 
+    static int nDaysInMonth; 
+    static int nDays; 
+    
 
-	public static void main(String args[]) { 
-    int givenYear = Integer.parseInt(args[0]);
-	int debugDaysCounter = 1; 
-	int countSunday = 0; 
-    curDay = 1;
-	curMonth = 1;
-	curYear = 1900; 
-    curDayOfWeek = 2; 
+    public static void main(String args[]) { 
+        int givenYear = Integer.parseInt(args[0]);
+        int debugDaysCounter = 1;
+        curDay = 1;
+        curMonth = 1;
+        curYear = 1900; 
+        curDayOfWeek = 2; 
 
-	while (curYear <= givenYear ) { 
-		if(curYear == givenYear) {
-		System.out.print(curDayOfWeek+"/"+curMonth+"/"+curYear); 
+        while (curYear < givenYear || (curYear == givenYear && curMonth <= 12)) { 
+            if (curYear == givenYear) 
+                System.out.print(curDay+"/"+curMonth+"/"+curYear); 
+            
+            if (debugDaysCounter % 7 == 0 && curYear == givenYear) 
+                System.out.print(" Sunday"); 
+            
+			if(curYear == givenYear) 	
+            System.out.println(); 
 
-		if (debugDaysCounter % 7 == 0) 
-		System.out.print(" Sunday"); 
+            advance();
 
-
-		System.out.println(); 
-		advance();
-		}
-		debugDaysCounter++; 
-	} 
-	
-	
-
-		}
-      
-		public static void advance() {
-            for(curMonth = 1; curMonth <= 12; curMonth++) {
-           
-			if(curDayOfWeek < 7)
-			curDayOfWeek++; 
-			else 
-			curDayOfWeek = 1; 
-
-
-			if (curDay < nDaysInMonth(curMonth, curYear)){
-			curDay++ ;
-			} else {
-			curDay = 1; 
-			curMonth++; 
-			}
-
-		}}
-
+            debugDaysCounter++; 
+        }
+    } 
+    
+    
+	public static void advance() {
+		if (curDayOfWeek < 7)
+			curDayOfWeek++;
+		else
+			curDayOfWeek = 1;
 		
-	private static boolean isLeapYear(int year) {
-	    boolean isLeapYear =  ( year % 400 == 0 || year % 4 == 0 && year % 100 != 0 ); 
-	   
-		return isLeapYear; 
-	}
+		if (curDay < nDaysInMonth(curMonth, curYear)) 
+			curDay++;
+		else {
+			curDay = 1;
+			curMonth++;
+			curDayOfWeek = 1;  
+		} 
 	
-
-	 
-	
-	private static int nDaysInMonth(int curMonthonth, int curYearear) {
-		int nDays;
-		switch (curMonthonth) {
-			case 4,6,9,11: 
-			nDays = 30;
-				break;
-
-			case 2: 
-			nDays = isLeapYear(curYear) ? 29 : 28;
-		       break;
-
-			case 1,3,5,7,8,10,12: 
-			nDays = 31; 
-			   break;
-
-			default: 
-			nDays = 0; 
-			System.out.println("Invalid month") ; 
-				break;
-		}
-		return nDays;
+		if (curMonth > 12){
+			curYear++;
+			curMonth = 1;
 	}
 }
+	
+    
+    private static boolean isLeapYear(int curYear) {
+        boolean isLeapYear =  (curYear % 400 == 0 || curYear % 4 == 0 && curYear % 100 != 0); 
+        return isLeapYear; 
+    }
 
+    private static int nDaysInMonth(int curMonth, int curYear) {
+        int nDays;
+        switch (curMonth) {
+            case 4, 6, 9, 11: 
+                nDays = 30;
+                break;
 
-		
+            case 2: 
+                nDays = isLeapYear(curYear) ? 29 : 28;
+                break;
+
+            case 1, 3, 5, 7, 8, 10, 12: 
+                nDays = 31; 
+                break;
+
+            default: 
+                nDays = 0; 
+                break;
+        }
+        return nDays;
+    }
+}
